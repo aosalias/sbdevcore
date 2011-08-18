06db88322c1bfb0be30898ac7500c9a70dac4bcc
Adam Olsen <aosalias@Im-On-Your-Network.local>
2011-08-18 16:47:44 -0600

    templates

--- lib/generators/core/install_generator.rb
+++ lib/generators/core/install_generator.rb
@@ -4,18 +4,18 @@ module Core
       source_root File.expand_path('../templates', __FILE__)
       argument :app_name, :type => :string
 
-      def database
-        copy_file "devise.rb", "config/initializers/devise.rb"
-        template  'database.yml', 'config/database.yml', :force => true
-        rake "db:drop:all"
-        rake "db:create"
-      end
-
       def remove_bs
         remove_file 'public/index.html'
         remove_file 'public/images/rails.png'
         remove_file 'app/views/layouts/application.html.erb'
         remove_file 'app/assets/stylesheets/application.css'
+      end
+
+      def database
+        directory "config", "config", :force => true
+        template  'database.yml', 'config/database.yml', :force => true
+        rake "db:drop:all"
+        rake "db:create"
       end
 
       def migrations
@@ -35,18 +35,12 @@ Index.find_all_by_name(APP_CONFIG[:static_pages]).each do |page|
       end
 
       def seeds
-        copy_file "load_app_config.rb", "config/initializers/load_app_config.rb"
-#        copy_file "application.html.haml", "app/views/layouts/application.html.haml"
         copy_file "seeds.rb", "db/seeds.rb", :force => true
 
-        copy_file "application.sass", "app/assets/stylesheets/application.sass", :force => true
-        copy_file "sass.sass", "app/assets/stylesheets/sass.sass", :force => true
+        copy_file "application.scss", "app/assets/stylesheets/application.scss", :force => true
+        copy_file "sass.scss", "app/assets/stylesheets/sass.scss", :force => true
         directory "application", "app/views/application"
-
-        copy_file "app_config.yml", "config/app_config.yml"
-        copy_file "tiny_mce.yml", "config/tiny_mce.yml", :force => true
-        copy_file "s3.yml", "config/s3.yml", :force => true
-        copy_file "mailer_setup.rb", "config/mailer_setup.rb", :force => true
+        directory "layouts", "app/views/layouts"
 
         copy_file "new_contact.html.erb", "app/views/contacts/new.html.erb", :force => true
         inject_into_file "app/assets/javascripts/application.js", "//= require core\n  ", :before => "//= require_tree ."

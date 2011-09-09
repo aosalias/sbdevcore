0935dcd91c13adac0c27b5bc8489bb3532fcdd25
Adam Olsen <aosalias@Im-On-Your-Network.local>
2011-09-08 19:09:55 -0600

    tablize

--- app/assets/javascripts/sbdevcore.js
+++ app/assets/javascripts/sbdevcore.js
@@ -7,9 +7,9 @@
 //= require jquery.tools.min.js
 //= require rails.validations
 //= require rails.validations.custom
-//= require tiny_mce_config.js
 //= require jquery.formalize
 //= require tinymce.jquery
+//= require tiny_mce_config.js
 //= require history.adapter.jquery
 //= require history
 //= require history.html4
